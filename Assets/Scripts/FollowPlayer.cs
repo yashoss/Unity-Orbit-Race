287e331756274ac778255ea039dd49bec0ab5ad2
Yasin <yashoss@gmail.com>
2020-06-28 09:53:57 -0700

    Add cinemachine to project and adjust settings.
    After trying to create our own smoothing camera lead I figured out how to install packages on unity. Added cinemachine to follow the player, but need to play around with numbers still. It isn't perfect, but it works decent.

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    
+    public Rigidbody2D playerRb;
+    /*
+    public float dampingFactor = 0.1f;
+    public float maxSpeed = 500.0f;
+    public float maxLeadingDistance = 5.0f;
+    public float minTime = 1f;
+    public float minMagnitude = 0.01f;
+    
+    private Vector3 zeroVector = Vector3.zero;
+    */
+    public float cameraVelocityFactor = 0.2f;
+    public float playerVelocityFactor = 1.2f;
+    public float maxAheadDistance = 2;
+    private Vector3 playerLastPosition;
+
+    private void Start()
+    {
+        playerLastPosition = playerRb.position;
+    }
+
+    void Update()
+    {
+
+    }
+}
